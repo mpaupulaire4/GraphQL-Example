2e9560e17bd640b51daeb6b5b0ff45776a61010e
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-04-28 09:59:56 -0600

    removed Firebase

--- src/Firebase/Models/index.js
+++
@@ -1,3 +0,0 @@
-export { default as User } from './User'
-export { default as Event } from './Event'
-export { default as Convo } from './Convo'
--- src/Firebase/index.js
+++
@@ -1,9 +0,0 @@
-import * as admin from 'firebase-admin';
-const cert = require('./service-account.json')
-
-export const FirebaseRef = admin.initializeApp({
-    credential: admin.credential.cert({
-        ...cert,
-    }),
-    databaseURL: "https://whos-down-5ed2d.firebaseio.com"
-});
