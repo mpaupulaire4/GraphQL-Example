5a17688f01f35fc88db6df41e06567397b2af725
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-05-04 12:04:03 -0600

    ConvoType

--- src/Data/ArangoDB/Convo.js
+++ src/Data/ArangoDB/Convo.js
@@ -5,7 +5,17 @@ import DataLoader from 'dataloader'
 /*::
   export type ConvoType = {
     id: string,
+    title: string,
+    participants: ParticipantsMap,
   };
+
+  type ParticipantsMap = {
+    [user_id: string]: ParticipantInfo,
+  }
+
+  type ParticipantInfo = {
+    timestamp: string,
+  }
 */
 
 export default class Convo extends Model/* :: <ConvoType> */{
