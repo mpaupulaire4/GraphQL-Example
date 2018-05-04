2bd4833354a0ea8ad35f3b43da2cba2878f93a54
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-05-04 11:54:58 -0600

    EventType

--- src/Data/ArangoDB/Event.js
+++ src/Data/ArangoDB/Event.js
@@ -4,8 +4,29 @@ import { Model, Node } from './'
 import DataLoader from 'dataloader'
 /*::
   export type EventType = {
-    id: string
+    id: string,
+    host: string,
+    title: string,
+    description: string,
+    time: string,
+    location: Location,
+    participants: ParticipantsMap,
+    visibility: Visibility,
   };
+
+  type Location = {
+    address?: string,
+    city?: string,
+    state?: string,
+    zip:? string,
+    coordinates: [number, number],
+  }
+
+  type ParticipantsMap = {
+    [user_id: string]: number,
+  }
+
+  type Visibility = 'PUBLIC' | 'PRIVATE' | 'NONE'
 */
 
 export default class Event extends Model/* :: <EventType> */{
