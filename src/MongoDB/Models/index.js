1ae8647082c1bbd47bb9cad856404cab360e5106
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-05-08 17:11:33 -0600

    removed Mongo Data

--- src/MongoDB/Models/index.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-import '../'
-export { default as User } from './User'
-export { default as Event } from './Event'
-export { default as Convo } from './Conversation'
-
-
